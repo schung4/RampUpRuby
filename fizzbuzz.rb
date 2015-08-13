943bd5e82d71a4ecaece17b1d1579efe0b7ad550
schung4 <samchung333@gmail.com>
2015-08-13 19:21:01 -0400

    fizzbuzz

---
+++ fizzbuzz.rb
@@ -0,0 +1,17 @@
+
+(1..100).each do |number|
+
+if number % 15 == 0
+	then print "fizzbuzz \n"
+
+elsif number % 5 == 0
+	then print "buzz \n"
+
+elsif number % 3 == 0
+	then print "fizz \n"
+
+else print "#{number} \n"
+
+end
+
+end
